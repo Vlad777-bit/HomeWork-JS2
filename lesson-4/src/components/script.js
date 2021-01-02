243b74af412ee31d7ada7c81fad1a6a595e18228
Vlad_Nozhkin <mega.nozhkin@mail.ru>
2021-01-02 17:48:33 +0300

    добавлен метод init

--- lesson-4/src/components/script.js
+++ lesson-4/src/components/script.js
@@ -1,16 +1,16 @@
 'use strict';
   
-class GoodsItem {
+class GoodsItem { 
     /**
      * 
+     * @param {number} id_product - id товара
      * @param {string} product_name - название товара
      * @param {number} price - цена товара
-     * @param {number} quantity - кол-во 
      */
-    constructor(product_name, price, quantity = 1) {
+    constructor(id_product, product_name, price) {
+        this.id_product = id_product;
         this.product_name = product_name;
         this.price = price;
-        this.quantity = quantity;
     }
 
     /**
@@ -21,9 +21,12 @@ class GoodsItem {
             <div class="item">
                 <img src="http://unsplash.it/180/150?random&gravity=center" alt="img">    
                 <h3>${this.product_name}</h3>
-                <span>Quantity - ${this.quantity}</span>  
                 <p>Price - ${this.price}</p>
-                <button class="btn item__btn">Добавить</button>
+                <button 
+                    class="btn item__btn"
+                    data-id="${this.id_product}"
+                    name="addToBasket"
+                >Добавить</button>
             </div>
         `;
     }
@@ -41,6 +44,14 @@ class Catalog {
     }
 
     /**
+     * Метод инициализирует все методы класса
+     */
+    init() {
+        this.getGoods();
+        this.handleEvents();
+    }
+
+    /**
      * Метод async getGoods:
      * Принимает url с псевдосервера
      * Переделывает его из json формата в object
@@ -52,7 +63,7 @@ class Catalog {
         try {
             const res = await fetch(this.url)
             const data = await res.json()
-            const getGoods = await data.map(item => new GoodsItem(item.product_name, item.price))
+            const getGoods = await data.map(item => new GoodsItem(item.id_product, item.product_name, item.price))
             const catalogList = getGoods.map(item => item.renderCatalog()).join('')
             document.querySelector('#catalog').insertAdjacentHTML('afterbegin', catalogList)
         } catch (e) {
@@ -73,17 +84,30 @@ class Catalog {
         //     document.querySelector('#catalog').insertAdjacentHTML('afterbegin', catalogList)
         // })     
     }
+
+    /**
+     * Метод позволяет при клике открывать корзину
+     */
+    handleEvents() {
+        const container = document.querySelector('#catalog');
+        const addTo = document.querySelector('.item__btn');
+      
+    }
 }
 
-class GoodsBasketItems extends GoodsItem {
+class BasketItems {
     /**
      * 
+     * @param {string} id_product - id товаров корзины
      * @param {string} product_name - название товаров корзины
      * @param {number} price - цена товаров корзины
      * @param {number} quantity - кол-во товаров корзины
      */
-    constructor(product_name, price, quantity) {
-        super(product_name, price, quantity);
+    constructor(id_product, product_name, price, quantity) {
+        this.id_product = id_product;
+        this.product_name = product_name;
+        this.price = price;
+        this.quantity = quantity;
     }
 
     /**
@@ -99,17 +123,29 @@ class GoodsBasketItems extends GoodsItem {
                     <h4>${this.product_name}</h4>
                     <span class="price">${this.quantity} * ${this.price}</span>
                 </div>
-                <button class="btn basket__del">X</button>
+                <button 
+                    class="btn basket__del"
+                    data-id="${this.id_product}"
+                    name="remove"
+                >X</button>
             </div>
         `;
     }
 }
 
-class Basket extends Catalog {
+class Basket {
     constructor(goods = [], amount = 0) {
-        super(goods)
+        this.goods = goods;
         this.amount = amount;
         this.url = 'https:raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/getBasket.json'
+    }
+
+    /**
+     * Метод инициализирует все методы класса
+     */
+    init() {
+        this.getGoods();
+        this.handleEvents();
     }
 
     /**
@@ -117,7 +153,7 @@ class Basket extends Catalog {
      * Принимает url с псевдосервера
      * Переделывает его из json формата в object
      * Записывает общую сумму товаров
-     * Распределяет товары для класса GoodsBasketItems
+     * Распределяет товары для класса BasketItems
      * Рендерит HTML разметку каталога
      * Записывает разметку в #catalog
      */
@@ -127,7 +163,7 @@ class Basket extends Catalog {
             const res = await fetch(this.url)
             const data = await res.json()
             const getAmmount = await data.amount
-            const getGoods = await data.contents.map(item => new GoodsBasketItems(item.product_name, item.price))
+            const getGoods = await data.contents.map(item => new BasketItems(item.id_product, item.product_name, item.price, item.quantity))
             const basketList = getGoods.map(item => item.renderBasket()).join('')
             document.querySelector('#basket').insertAdjacentHTML('afterbegin', basketList)
             document.querySelector('.amount').innerText = getAmmount;
@@ -158,12 +194,11 @@ class Basket extends Catalog {
 }
 
 const catalog = new Catalog();
-catalog.getGoods(); 
+catalog.init(); 
 
 
 const basket = new Basket();
-basket.getGoods();
-basket.handleEvents();
+basket.init();
 
 
 
